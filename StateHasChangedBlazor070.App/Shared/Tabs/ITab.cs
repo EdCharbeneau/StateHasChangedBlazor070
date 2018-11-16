23a0e5b48e11d3579537b757860bccc079732d3c
Ed Charbeneau <ed.charbeneau@gmail.com>
2018-11-16 13:01:47 -0500

    implemented cascading tabs

---
+++ StateHasChangedBlazor070.App/Shared/Tabs/ITab.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Blazor;
+
+namespace StateHasChangedBlazor070.App.Shared.Tabs
+{
+    public interface ITab
+    {
+        RenderFragment ChildContent { get; }
+    }
+}
