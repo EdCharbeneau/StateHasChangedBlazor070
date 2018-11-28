199ee1e0b5ea9330befd66f7a79961efb68e0370
Ed Charbeneau <ed.charbeneau@gmail.com>
2018-11-27 19:50:37 -0500

    Moved "code-behind" to class files. Included .filenexting.json for nesting components in IDEs

---
+++ StateHasChangedBlazor070.Components/Tabs/Tab.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Components;
+using System;
+
+namespace StateHasChangedBlazor070.Components.Tabs
+{
+    public class TabComponent : BlazorComponent, ITab, IDisposable
+    {
+        [CascadingParameter] protected TabSet ContainerTabSet { get; set; }
+        [Parameter] protected string Title { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; private set; }
+
+        protected bool IsActive => ContainerTabSet.ActiveTab == this;
+        protected string ActiveCssClass => IsActive ? "active" : String.Empty;
+
+        protected override void OnInit()
+        {
+            ContainerTabSet.AddTab(this);
+        }
+
+        public void Dispose()
+        {
+            ContainerTabSet.RemoveTab(this);
+        }
+
+        protected void Activate()
+        {
+            ContainerTabSet.SetActiveTab(this);
+        }
+    }
+}
---
+++ StateHasChangedBlazor070.Components/Tabs/TabSet.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Components;
+using System;
+using System.Collections.Generic;
+
+namespace StateHasChangedBlazor070.Components.Tabs
+{
+    public class TabSetComponent : BlazorComponent
+    {
+        [Parameter] protected RenderFragment ChildContent { get; set; }
+
+        [Parameter] protected int DefaultTab { get; set; }
+
+        [Parameter]
+        protected int Selected
+        {
+            get
+            {
+                return selected.Value;
+            }
+            set
+            {
+                if (value >= 0 && value <= tabs.Count - 1)
+                {
+                    SetActiveTab(tabs[value]);
+                }
+            }
+        }
+
+        [Parameter] protected Action<int> SelectedChanged { get; set; }
+
+        public ITab ActiveTab { get; private set; }
+
+        protected int? selected;
+
+        protected List<ITab> tabs = new List<ITab>();
+
+        public void AddTab(ITab tab)
+        {
+            tabs.Add(tab);
+            if (ActiveTab == null || tabs.Count - 1 == DefaultTab)
+            {
+                SetActiveTab(tab);
+            }
+        }
+
+        public void RemoveTab(ITab tab)
+        {
+            tabs.Remove(tab);
+            if (ActiveTab == tab)
+            {
+                SetActiveTab(null);
+            }
+        }
+
+        public void SetActiveTab(ITab tab)
+        {
+            if (ActiveTab != tab)
+            {
+                ActiveTab = tab;
+                selected = tabs.IndexOf(tab);
+                SelectedChanged?.Invoke(selected.Value);
+                StateHasChanged();
+            }
+        }
+    }
+}
