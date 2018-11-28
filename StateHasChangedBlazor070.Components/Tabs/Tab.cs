64df02cb8ab49138e10ce40a45e4fc335aeee8bd
Ed Charbeneau <ed.charbeneau@gmail.com>
2018-11-27 20:05:48 -0500

    Added ///Summary for better intellisense

--- StateHasChangedBlazor070.Components/Tabs/Tab.cs
+++ StateHasChangedBlazor070.Components/Tabs/Tab.cs
@@ -6,9 +6,19 @@ namespace StateHasChangedBlazor070.Components.Tabs
 {
     public class TabComponent : BlazorComponent, ITab, IDisposable
     {
+        // Set by CascadingValue Component
         [CascadingParameter] protected TabSet ContainerTabSet { get; set; }
-        [Parameter] protected string Title { get; set; }
+
+#pragma warning disable BL9993 // Component parameter is marked public due to ITab requirement
         [Parameter] public RenderFragment ChildContent { get; private set; }
+#pragma warning restore BL9993 // Component parameter is marked public
+
+
+        [Parameter]         
+        /// <summary>
+        /// The Tab Title
+        /// </summary>
+        protected string Title { get; set; }
 
         protected bool IsActive => ContainerTabSet.ActiveTab == this;
         protected string ActiveCssClass => IsActive ? "active" : String.Empty;
--- StateHasChangedBlazor070.Components/Tabs/TabSet.cs
+++ StateHasChangedBlazor070.Components/Tabs/TabSet.cs
@@ -9,9 +9,16 @@ namespace StateHasChangedBlazor070.Components.Tabs
     {
         [Parameter] protected RenderFragment ChildContent { get; set; }
 
-        [Parameter] protected int DefaultTab { get; set; }
+        [Parameter]
+        /// <summary>
+        /// Sets the default tab when the component initializes.
+        /// </summary>
+        protected int DefaultTab { get; set; }
 
         [Parameter]
+        /// <summary>
+        /// Gets or Sets the current tab selection.
+        /// </summary>
         protected int Selected
         {
             get
@@ -27,14 +34,25 @@ namespace StateHasChangedBlazor070.Components.Tabs
             }
         }
 
-        [Parameter] protected Action<int> SelectedChanged { get; set; }
+        [Parameter]
+        /// <summary>
+        /// The Action invoked when the Selected value is changed.
+        /// </summary>
+        protected Action<int> SelectedChanged { get; set; }
 
+        /// <summary>
+        /// Active tab state consumed by child tab components
+        /// </summary>
         public ITab ActiveTab { get; private set; }
 
         protected int? selected;
 
         protected List<ITab> tabs = new List<ITab>();
 
+        /// <summary>
+        /// Registers a Tab within a TabSet
+        /// </summary>
+        /// <param name="tab"></param>
         public void AddTab(ITab tab)
         {
             tabs.Add(tab);
@@ -44,6 +62,10 @@ namespace StateHasChangedBlazor070.Components.Tabs
             }
         }
 
+        /// <summary>
+        /// Removes a Tab within a TabSet
+        /// </summary>
+        /// <param name="tab"></param>
         public void RemoveTab(ITab tab)
         {
             tabs.Remove(tab);
@@ -53,6 +75,10 @@ namespace StateHasChangedBlazor070.Components.Tabs
             }
         }
 
+        /// <summary>
+        /// Sets the active Tab
+        /// </summary>
+        /// <param name="tab"></param>
         public void SetActiveTab(ITab tab)
         {
             if (ActiveTab != tab)
